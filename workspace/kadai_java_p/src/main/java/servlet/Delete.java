4e094440a320fdb785142013f41f2a93e862098c
asuen39 <aiegdnmissi.2405@gmail.com>
2022-09-25 17:00:10 +0900

    Delete.javaを新たに作成

--- workspace/kadai_java_p/src/main/java/servlet/Delete.java
+++ workspace/kadai_java_p/src/main/java/servlet/Delete.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,22 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Dao.CorrectAnswersBean;
 import Dao.CorrectAnswersDao;
-import Dao.QuestionsBean;
 import Dao.QuestionsDao;
 
 /**
  * Servlet implementation class Top
  */
 @WebServlet("/delete_confirm")
-public class DeleteConfirm extends HttpServlet {
+public class Delete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DeleteConfirm() {
+    public Delete() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -37,32 +34,24 @@ public class DeleteConfirm extends HttpServlet {
 		// 文字コードの指定
 	    request.setCharacterEncoding("utf-8");
 	    
-	    //list 削除ボタンからの値を取得
-	    int delete_id = Integer.parseInt(request.getParameter("delete_id"));
+	    
+	    //削除実行ボタンからの値取得
+	    int execute_delete = Integer.parseInt(request.getParameter("execute_delete"));
 	    
 	    
 	    try {
 	    	//問題一覧取得
 			QuestionsDao dao = new QuestionsDao();
 	    	
-			//削除のIDを問題一覧のIDと比較
-	    	QuestionsBean questionsBean = dao.find(delete_id);
-	    	
-	    	//コンソールに表示するだけ。確認用
-	    	//questionsBean.outputData();
-	    	
-	    	//問題一覧設置
-	    	request.setAttribute("questionList", questionsBean);
-	    	
-	    	
-	    	//回答一覧取得
+			//回答一覧取得
 	    	CorrectAnswersDao dao_answer = new CorrectAnswersDao();
-	    	ArrayList<CorrectAnswersBean> answerBean = (ArrayList<CorrectAnswersBean>) dao_answer.findByQuetionsId(delete_id);
+	    	//ArrayList<CorrectAnswersBean> answerBean = (ArrayList<CorrectAnswersBean>) dao_answer.findByQuetionsId(delete_id);
 	    	
-	    	//回答一覧設置
-	    	request.setAttribute("answerList", answerBean);
+	    	//削除メソッド呼び出し
+	    	int delete = dao.delete(execute_delete);
 	    	
-	    	
+	    	//削除実行
+	    	request.setAttribute("delete", delete);
 	    	
 	    } catch (Exception e) {
 			e.printStackTrace();
