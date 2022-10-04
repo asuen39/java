2fa221ee53bb8cb6ef59fccc3a8d8d6821a408df
asuen39 <aiegdnmissi.2405@gmail.com>
2022-10-04 13:30:08 +0900

    文字条件文修正

--- workspace/kadai_java_p/src/main/java/servlet/register/Confirm.java
+++ workspace/kadai_java_p/src/main/java/servlet/register/Confirm.java
@@ -89,7 +89,8 @@ public class Confirm extends HttpServlet {
 	  	for( int i = 0; i < answer.length; i++){
 	  		//答え欄3つあるが1つだけ入力された状態等の空きinputが送られてくる場合、
 	  		//空きinputをここで排除する。全部空きinputだったらfor文から抜けてエラー文が実行される。
-	  		if(answer[i] != "") {
+	  		//equalsの判定にする事で文字の判定に抜けが無いようにする。
+	  		if(answer[i].equals(answer[i])) {
 	  			//System.out.println(answer[i]);
 	  			
 	  			//答えが入力を確認出来たら実行する。
